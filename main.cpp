08be6abeca496ae65bdb0912ce24d8b0df4d56f8
DaikiWakui <k021g1119@g.neec.ac.jp>
2022-11-29 13:16:16 +0900

    リスト表示まで

--- main.cpp
+++ main.cpp
@@ -7,10 +7,19 @@
 using namespace std;
 
 int main() {
+	list<const char*> lst(0);
+	auto itr = lst.begin();
+	itr = lst.insert(itr, "tuika");
+	++itr;
+	itr = lst.insert(itr, "tuika2");
+	++itr;
+	itr = lst.insert(itr, "tuika3");
+
 	int state = 0;
 
 	int isDraw[3] = {};
-	int userInput = 0;
+	int userInput[3] = {};
+	char str[16] = {};
 
 	while (true)
 	{
@@ -25,44 +34,85 @@ int main() {
 			printf("5.要素の並び替え(オプション)\n");
 			printf("-------------------------------\n");
 			printf("操作を選択して下さい\n");
-			scanf_s("%d", &userInput);
-			state = userInput;
+			scanf_s("%d", &userInput[0]);
+			state = userInput[0];
 		}
 
 		if (state == 1) {
-			
+			//要素の表示
 			if (isDraw[0] == 0) {
 				system("cls");
-				userInput = 0;
+				userInput[0] = 0;
 				printf("[要素の表示]\n");
 				printf("1.要素の一覧表示\n");
 				printf("2.順番を指定して要素を表示\n");
 				printf("9.要素操作に戻る\n");
 				printf("\n操作を選択して下さい\n");
-				scanf_s("%d", &userInput);
+				scanf_s("%d", &userInput[0]);
 				isDraw[0] = 1;
 			}
-			
-			if (userInput == 1) {
-				if (isDraw[1] == 0) {
-					system("cls");
-					printf("[要素の一覧表示]\n");
-					isDraw[1] = 1;
+
+			//要素の一覧表示
+			if (userInput[0] == 1 && isDraw[1] == 0) {
+				system("cls");
+				printf("[要素の一覧表示]\n");
+				printf("要素一覧 : {\n");
+
+				int i = 0;
+
+				for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+					printf(" %d : ", i);
+					cout << *itr << "\n";
+					i++;
 				}
 
-				scanf_s("%d", &userInput);
+				printf("}\n");
+
+				printf("要素数 : ");
+				cout << lst.size() << "\n";
+
+				printf("\n-------------------------------\n");
+				printf("1.要素の表示に戻る\n");
+				printf("2.要素の操作に戻る\n");
+
+				scanf_s("%d", &userInput[1]);
+
+				isDraw[1] = 1;
 			}
 
-			if (userInput == 2) {
-				if (isDraw[2] == 0) {
-					system("cls");
-					printf("[順番を指定して要素を表示]\n");
-					isDraw[2] = 1;
+			//順番を指定して要素を表示
+			if (userInput[0] == 2&& isDraw[2] == 0) {
+				system("cls");
+				printf("[順番を指定して要素を表示]\n");
+
+				printf("表示したい要素の順番を指定してください。\n");
+
+				//ここに入力された要素の出力
+				scanf_s("%d", &userInput[2]);
+				printf("要素番号 %d : ", userInput[2]);
+				itr = lst.begin();
+				for (int i = 0; i < userInput[2]; i++) {
+					++itr;
 				}
+				cout << *itr << "\n";
 
+				printf("\n-------------------------------\n");
+				printf("1.要素の表示に戻る\n");
+				printf("2.要素の操作に戻る\n");
+
+				scanf_s("%d", &userInput[1]);
+
+				isDraw[2] = 1;
 			}
 
-			if (userInput == 9) {
+			if (userInput[1] == 1) {
+				isDraw[0] = 0;
+				isDraw[1] = 0;
+				isDraw[2] = 0;
+			}
+
+			//要素操作に戻る
+			if (userInput[0] == 9 || userInput[1] == 2) {
 				isDraw[0] = 0;
 				isDraw[1] = 0;
 				isDraw[2] = 0;
@@ -70,20 +120,43 @@ int main() {
 			}
 		}
 
+		//要素の挿入
 		if (state == 2) {
-			printf("要素の挿入\n");
+			if (isDraw[0] == 0) {
+				system("cls");
+				printf("[リスト要素の挿入]\n");
+				printf("要素を追加する場所を指定してください。\n");
+				scanf_s("%d", &userInput[0]);
+
+				printf("追加する要素の値を入力してください。\n");
+				scanf_s("%s", str, 16);
+				const char* newstr;
+				newstr = str;
+
+				printf("要素 %s が %d 番目に挿入されました。\n", str, userInput[0]);
+
+
+				printf("\n9.要素操作に戻る\n");
+				scanf_s("%d", &userInput[2]);
+				isDraw[0] = 1;
+			}
+			
+			if (userInput[2] == 9) {
+				isDraw[0] = 0;
+				state = 0;
+			}
 		}
 
 		if (state == 3) {
-			printf("要素の表示\n");
+			printf("要素の編集\n");
 		}
 
 		if (state == 4) {
-			printf("要素の表示\n");
+			printf("要素の削除\n");
 		}
 
 		if (state == 5) {
-			printf("要素の表示\n");
+			printf("要素の並び替え\n");
 		}
 	}
 	
